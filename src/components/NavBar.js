745bc64f9adc57b83da3da31a2b72c70f6cedfea
Brian Ashman <brian.ashman@comcast.net>
2023-06-17 16:02:13 -0700

    Complete React Routes Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,23 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <NavLink to="/" exact>
+      Home
+    </NavLink>
+    <br></br>
+    <NavLink to="/Movies" exact>
+      Movies
+    </NavLink>
+    <br></br>
+    <NavLink to="/Directors" exact>
+      Directors
+    </NavLink>
+    <br></br>
+    <NavLink to="/Actors" exact>
+      Actors
+    </NavLink>
+  </div>;
 }
 
 export default NavBar;
